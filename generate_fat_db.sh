5444ba0507bc25ca4f7ab9a914285de5fe29233c
Ecmel Berk Canlier <me@ecmelberk.com>
2020-07-03 22:20:46 +0300

    Make the generator a little more immune from force pushes & the like

--- generate_fat_db.sh
+++ generate_fat_db.sh
@@ -3,81 +3,86 @@ set -euo pipefail
 
 # might not work for non-github repos, fix that
 sanitize_folder_name() {
-	echo "$1" | cut -d"/" -f4- | sed 's@/@_@'
+    echo "$1" | cut -d"/" -f4- | sed 's@/@_@'
 }
 
 get_path_in_repo() {
-	echo "$1" | cut -d"/" -f2-
+    echo "$1" | cut -d"/" -f2-
 }
 
 # just *hope* no one puts an executable named "build" anywhere other than the package dirs
 find_packages() {
-	find "$1" -name build -type f -executable -exec dirname {} \;
+    find "$1" -name build -type f -executable -exec dirname {} \;
 }
 
 fetch_repo() {
-	REPO="$1"
-	FOLDER="$2"
+    REPO="$1"
+    FOLDER="$2"
 
-	if [ -d "$FOLDER" ]; then
-		echo "  -> Pulling"
+    if [ -d "$FOLDER" ]; then
+        echo "  -> Pulling"
 
-		cd "$FOLDER"
-		git pull
-		cd ".."
-	else
-		echo "  -> Cloning"
+        cd "$FOLDER"
+        git pull -f
+        cd ".."
+    else
+        echo "  -> Cloning"
 
-		git clone "$REPO" "$FOLDER"
-	fi
+        git clone "$REPO" "$FOLDER"
+
+        cd "$FOLDER"
+        git config user.email "kissfind@mail.invalid"
+        git config user.name "kiss-find Database"
+        cd ".."
+    fi
 }
 
 process_repo() {
-	REPO="$1"
-	FOLDER="$2"
+    REPO="$1"
+    FOLDER="$2"
 
-	find_packages "$FOLDER" | while read PACKAGE; do
-		PPATH="$(get_path_in_repo "$PACKAGE")"
+    find_packages "$FOLDER" | while read PACKAGE; do
+    PPATH="$(get_path_in_repo "$PACKAGE")"
 
-		echo "  -> Found package $PACKAGE"
+    echo "  -> Found package $PACKAGE"
 
-		if [ -L "$PACKAGE/version" ]; then
-			# not sure how to handle symlinks in a proper way yet
-			VERSION="symlink"
-		else
-			VERSION="$(cat "$PACKAGE/version")"
-		fi
+    if [ -L "$PACKAGE/version" ]; then
+        # not sure how to handle symlinks in a proper way yet
+        VERSION="symlink"
+    else
+        VERSION="$(cat "$PACKAGE/version")"
+    fi
 
-		if [ -f "$PACKAGE/description" ]; then
-		    if [ -L "$PACKAGE/description" ]; then
-    			DESCRIPTION="symlink"
-	    	else
-		    	DESCRIPTION="$(cat "$PACKAGE/description")"
-    		fi
-		else
-			DESCRIPTION=""
-		fi
+    if [ -f "$PACKAGE/description" ]; then
+        if [ -L "$PACKAGE/description" ]; then
+            DESCRIPTION="symlink"
+        else
+            DESCRIPTION="$(cat "$PACKAGE/description")"
+        fi
+    else
+        DESCRIPTION=""
+    fi
 
-		jq -c -n \
-			--arg version "$VERSION" \
-			--arg package "$(basename "$PACKAGE")" \
-			--arg repo "$REPO" \
-			--arg path "$PPATH" \
-			--arg description "$DESCRIPTION" \
-			'{"package":$package,"version":$version,"repo":$repo,"path":$path,"description":$description}' \
-			>> ../packages.json
-	done
+    jq -c -n \
+        --arg version "$VERSION" \
+        --arg package "$(basename "$PACKAGE")" \
+        --arg repo "$REPO" \
+        --arg path "$PPATH" \
+        --arg description "$DESCRIPTION" \
+        '{"package":$package,"version":$version,"repo":$repo,"path":$path,"description":$description}' \
+        >> ../packages.json
+    done
 }
 
 mkdir -p "repos"
 cat "$1" | while read REPO; do
-	cd repos
+cd repos
 
-	FOLDER="$(sanitize_folder_name "$REPO")"
-	echo ":: $REPO ($FOLDER)"
+FOLDER="$(sanitize_folder_name "$REPO")"
+echo ":: $REPO ($FOLDER)"
 
-	fetch_repo "$REPO" "$FOLDER"
-	process_repo "$REPO" "$FOLDER"
+fetch_repo "$REPO" "$FOLDER"
+process_repo "$REPO" "$FOLDER"
 
-	cd ..
+cd ..
 done
