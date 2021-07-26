08e299122692acf7de168fae446a4d6a6613f903
Jonathan Dahan <git@jonathan.is>
2021-07-26 01:08:22 -0400

    add long form options --update, --help

--- dist/kiss/kiss-find/files/kiss-find.sh
+++ dist/kiss/kiss-find/files/kiss-find.sh
@@ -4,26 +4,7 @@
 VERSION="2"
 DB_PATH="${XDG_CACHE_HOME:-${HOME}/.cache}"/kiss-find/db
 UPDATE_URL="https://github.com/jedahan/kiss-find/releases/download/latest/db"
-
 mkdir -p "$(dirname "${DB_PATH}")"
-
-[ ! -f "${DB_PATH}" ] && show_update && exit
-(( $(date -r ~/.cache/kiss-find/db -v+7d +%s) < $(date +%s) )) && show_update
-
-if [ -z "$1" ] || [ "$1" = "-h" ] || [ "$1" = "--help" ]; then show_help; exit; fi
-if [ "$1" = "-u" ] || [ "$1" = "--update" ]; then update; exit; fi
-
-update() {
-  command -v curl >/dev/null || command -v wget >/dev/null || {
-    echo "please install curl or wget to update" >&2 && exit
-  }
-
-  command -v curl >/dev/null \
-    && command curl --location --silent --user-agent "kiss-find/${VERSION}" "${UPDATE_URL}" --output "${DB_PATH}" \
-    || command wget -U "kiss-find/${VERSION}" "${UPDATE_URL}" -O "${DB_PATH}"
-
-  echo ":: Update done" >&2 && exit
-}
 
 show_help() {
   echo "kiss-find ${VERSION}"
@@ -38,11 +19,25 @@ show_update() {
   echo >&2
 }
 
-_grep=${KISS_FIND_GREP:-"$(command -v rg || command -v ag || command -v ack || command -v grep)"}
-[ -z "$grep" ] && echo "no grep found" >&2 && exit
+update() {
+  command -v curl >/dev/null || command -v wget >/dev/null || {
+    echo "please install curl or wget to update" >&2 && exit
+  }
 
-if [ -t 1 ]; then
-  "$_grep" "$@" "${DB_PATH}" | sort | column -t -s','
-else
-  "$_grep" "$@" "${DB_PATH}" | sort
-fi
+  command -v curl >/dev/null \
+    && command curl --location --silent --user-agent "kiss-find/${VERSION}" "${UPDATE_URL}" --output "${DB_PATH}" \
+    || command wget -U "kiss-find/${VERSION}" "${UPDATE_URL}" -O "${DB_PATH}"
+
+  echo ":: Update done" >&2 && exit
+}
+
+[ -f "${DB_PATH}" ] && (( $(date -r ~/.cache/kiss-find/db -v+7d +%s) < $(date +%s) )) && show_update
+if [ -z "$1" ] || [ "$1" = "-h" ] || [ "$1" = "--help" ]; then show_help; exit; fi
+if [ "$1" = "-u" ] || [ "$1" = "--update" ]; then update; exit; fi
+[ ! -f "${DB_PATH}" ] && show_update && exit
+
+_grep=${KISS_FIND_GREP:-"$(command -v rg || command -v ag || command -v ack || command -v grep)"}
+[ -z "$_grep" ] && echo "no grep found" >&2 && exit
+
+results=$("$_grep" "$@" "${DB_PATH}" | sort)
+[ -t 1 ] && echo "$results" | column -t -s',' || echo "$results"
