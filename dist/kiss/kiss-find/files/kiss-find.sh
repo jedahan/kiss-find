96a135a89176cb035ba943b9eb1488f44e1da8cc
Jonathan Dahan <git@jonathan.is>
2021-07-26 00:58:17 -0400

    split into functions

--- dist/kiss/kiss-find/files/kiss-find.sh
+++ dist/kiss/kiss-find/files/kiss-find.sh
@@ -1,46 +1,45 @@
 #!/bin/sh -e
 # Search for packages across every known repository
 
-VERSION="1"
+VERSION="2"
 DB_PATH="${XDG_CACHE_HOME:-${HOME}/.cache}"/kiss-find/db
 UPDATE_URL="https://github.com/jedahan/kiss-find/releases/download/latest/db"
 
 mkdir -p "$(dirname "${DB_PATH}")"
 
-if [ -z "$1" ] || [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
-  echo "kiss-find ${VERSION}"
-  echo "$0 <query> :: Search for packages across every known repository"
-  echo "$0 -u      :: Update package database"
-  exit
-elif [ "$1" = "-u" ]; then
+[ ! -f "${DB_PATH}" ] && show_update && exit
+(( $(date -r ~/.cache/kiss-find/db -v+7d +%s) < $(date +%s) )) && show_update
+
+if [ -z "$1" ] || [ "$1" = "-h" ] || [ "$1" = "--help" ]; then show_help; exit; fi
+if [ "$1" = "-u" ] || [ "$1" = "--update" ]; then update; exit; fi
+
+update() {
   command -v curl >/dev/null || command -v wget >/dev/null || {
     echo "please install curl or wget to update" >&2 && exit
   }
 
-  command -v curl >/dev/null && \
-    command curl --location --silent \
-      --user-agent "kiss-find/${VERSION}" \
-      "${UPDATE_URL}" \
-      --output "${DB_PATH}" || \
-    command wget -U "kiss-find/${VERSION}" "${UPDATE_URL}" -O "${DB_PATH}"
+  command -v curl >/dev/null \
+    && command curl --location --silent --user-agent "kiss-find/${VERSION}" "${UPDATE_URL}" --output "${DB_PATH}" \
+    || command wget -U "kiss-find/${VERSION}" "${UPDATE_URL}" -O "${DB_PATH}"
 
-  echo ":: Update done" >&2
+  echo ":: Update done" >&2 && exit
+}
+
+show_help() {
+  echo "kiss-find ${VERSION}"
+  echo "$0 <query>         :: Search for packages across every known repository"
+  echo "$0 -u, --update    :: Update package database"
+  echo "$0 -h, --help      :: Show this help"
   exit
-fi
+}
 
-set -u # was required for the -z check above, can enable now
+show_update() {
+  echo ':: Please run `kiss find --update` to download the latest database' >&2
+  echo >&2
+}
 
-if [ ! -f "${DB_PATH}" ]; then
-  echo ":: Please run with '-u' to update" >&2
-  exit
-fi
-
-_grep=${KISS_FIND_GREP:-"$(
-  command -v rg ||
-  command -v ag ||
-  command -v ack ||
-  command -v grep
-)"} || _grep=grep
+_grep=${KISS_FIND_GREP:-"$(command -v rg || command -v ag || command -v ack || command -v grep)"}
+[ -z "$grep" ] && echo "no grep found" >&2 && exit
 
 if [ -t 1 ]; then
   "$_grep" "$@" "${DB_PATH}" | sort | column -t -s','
