ed0cf7ff5b98247c37579c887ccd34f719bd384b
Jonathan Dahan <git@jonathan.is>
2021-07-25 22:34:24 -0400

    sync_latest_repos.sh: port from bash to shell

--- lib/sync_latest_repos.sh
+++ lib/sync_latest_repos.sh
@@ -1,19 +1,22 @@
-#!/usr/bin/env bash
-set -euo pipefail
+#!/bin/sh -e
 
 warn() {
   echo "$@" >&2
 }
 
+clean() {
+  rm -f .safelist .githublist .fulllist .blocklist
+}
+trap clean EXIT INT
+
 current_dir=$(dirname "$0")
 
-command -v gh >/dev/null || warn 'gh commandline not found, please install to discover kiss-repo repositories on github'
+clean
+grep -v '^[# ].*' "${current_dir}"/../repo_blocklist | sort -u > .blocklist
+grep -v '^[# ].*' "${current_dir}"/../repo_safelist | sort -u > .safelist
+
+command -v gh 2>/dev/null || warn 'gh commandline not found, please install to discover kiss-repo repositories on github'
 gh auth status 2>/dev/null || warn 'gh auth status error, please login with gh auth login'
-gh auth status 2>/dev/null && \
-  githublist=$(gh api graphql --field query=@"${current_dir}"/get_gh_repos.gql | jq -r ".data.search.edges[].node.url" | sort)
+gh auth status 2>/dev/null && gh api graphql --field query=@"${current_dir}"/get_gh_repos.gql | jq -r ".data.search.edges[].node.url" | sort -u >> .safelist
 
-safelist=$(grep -v '^[# ].*' "${current_dir}"/../repo_safelist | sort)
-
-comm -23 \
-  <(printf '%s\n%s' "$safelist" "$githublist" | sort) \
-  <(grep -v '^[# ].*' "${current_dir}"/../repo_blocklist | sort)
+comm -23 .safelist .blocklist
