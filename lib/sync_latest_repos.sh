88ab5eb042c48655e7b78e84b71ffb4f5a7b92e8
Jonathan Dahan <git@jonathan.is>
2021-08-10 12:46:48 -0400

    sync_latest_repos: print out # of found repositories

--- lib/sync_latest_repos.sh
+++ lib/sync_latest_repos.sh
@@ -5,18 +5,22 @@ warn() {
 }
 
 clean() {
-  rm -f .include .githublist .fulllist .filter
+  rm -f .include .githublist .fulllist .filter .results
 }
 trap clean EXIT INT
-
-current_dir=$(dirname "$0")
-
 clean
-grep -v '^[# ].*' "${current_dir}"/../filter | sort -u > .filter
-grep -v '^[# ].*' "${current_dir}"/../include | sort -u > .include
+
+libdir=$(dirname "$0")
+
+grep -v '^[# ].*' "${libdir}"/../filter | sort -u > .filter
+grep -v '^[# ].*' "${libdir}"/../include | sort -u > .include
 
 command -v gh >/dev/null || warn 'gh commandline not found, please install to discover kiss-repo repositories on github'
 gh auth status 2>/dev/null || warn 'gh auth status error, please login with gh auth login'
-gh auth status 2>/dev/null && gh api graphql --field query=@"${current_dir}"/get_gh_repos.gql | jq -r ".data.search.edges[].node.url" | sort -u >> .include
+gh auth status 2>/dev/null && {
+  gh api graphql --field query=@"${libdir}"/get_gh_repos.gql > .results
+  echo "$(jq -r '.data.search.repositoryCount' .results)" repositories found
+  jq -r '.data.search.edges[].node.url' .results | sort -u >> .include
+}
 
 comm -23 .include .filter
